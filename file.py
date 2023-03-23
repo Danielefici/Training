4084ad48987164080d4716ae72e9431648a21287
Daniele <danieleficili@gmail.com>
2023-03-23 15:04:00 +0100

    bbbb

---
+++ file.py
@@ -0,0 +1,13 @@
+def somma(a:float,b:float):
+    '''questa funct blabla'''
+    s=a+b 
+    print(s)
+    return s 
+
+def main():
+    a=5
+    B=6
+    somma(a,b)
+
+if __name__ == "__main__":
+    main()
